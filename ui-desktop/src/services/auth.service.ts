d3141c94e440aa863ea675225ecc9c7a2be12078
Salih52 <saliheliack@gmail.com>
2024-09-21 15:32:44 +0300

    fix

--- ui-desktop/src/services/auth.service.ts
+++ ui-desktop/src/services/auth.service.ts
@@ -23,7 +23,7 @@ export class VeriService{
     private router: Router,
     private cookieService: CookieService
   ) {
-    this.url ='http://localhost:8080/api/v1/auth';
+    this.url ='http://backend:8080/api/v1/auth';
     }
   public register(veri: AuthResponse) {
     veri.role = 'user';
