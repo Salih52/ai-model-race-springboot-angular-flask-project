87e298a196ce7d27642b688a03fa218db71a77c3
Salih52 <saliheliack@gmail.com>
2024-10-06 19:04:07 +0300

    fix

--- ui-desktop/src/services/environment.prod.ts
+++ ui-desktop/src/services/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment_auth = {
     production: true,
-    apiUrl: 'http://ailabrace.online/api/v1/auth',
+    apiUrl: 'https://ailabrace.online/api/v1/auth',
     websocketUrl: 'wss://ailabrace.online/ng-cli-ws' // Adjust as needed
   };
   
