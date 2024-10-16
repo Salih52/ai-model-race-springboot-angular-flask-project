6e278af588c003b62332c473d248de8eec376a23
Salih52 <saliheliack@gmail.com>
2024-10-16 16:54:51 +0300

    a

--- backend/src/main/java/com/example/backend/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/backend/config/JwtAuthenticationFilter.java
@@ -17,46 +17,46 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-//@Component
-//@RequiredArgsConstructor
-//public class JwtAuthenticationFilter extends OncePerRequestFilter {
-//    private final JwtService jwtService;
-//    private final UserDetailsService userDetailsService;
-//    private final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
-//
-//    @Override
-//    protected void doFilterInternal(
-//            @NonNull HttpServletRequest request,
-//            @NonNull HttpServletResponse response,
-//            @NonNull FilterChain filterChain
-//    ) throws ServletException, IOException {
-//        final String authHeader = request.getHeader("Authorization");
-//        final String jwt;
-//        final String userName;
-//
-//        logger.info("autheader:" + authHeader);
-//        jwt = authHeader.substring(7);
-//        userName = jwtService.extractUserName(jwt);
-//
-//        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userName);
-//            if (jwtService.isTokenValid(jwt, userDetails)) {
-//                // Token geçerli
-//                logger.info("Token is valid for user: {}", userName);
-//                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-//                        userDetails,
-//                        null,
-//                        userDetails.getAuthorities()
-//                );
-//                authenticationToken.setDetails(
-//                        new WebAuthenticationDetailsSource().buildDetails(request)
-//                );
-//                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-//            } else {
-//                logger.warn("Invalid JWT token for user: {}", userName);
-//            }
-//        }
-//
-//        filterChain.doFilter(request, response);
-//    }
-//}
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    private final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userName;
+
+        logger.info("autheader:" + authHeader);
+        jwt = authHeader.substring(7);
+        userName = jwtService.extractUserName(jwt);
+
+        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userName);
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                // Token geçerli
+                logger.info("Token is valid for user: {}", userName);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            } else {
+                logger.warn("Invalid JWT token for user: {}", userName);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
