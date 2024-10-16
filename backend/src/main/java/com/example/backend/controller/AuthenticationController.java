f1eaeb9b6ed3648fe2409ec784b9d67144727e16
Salih52 <saliheliack@gmail.com>
2024-10-17 01:03:20 +0300

    fix

--- backend/src/main/java/com/example/backend/controller/AuthenticationController.java
+++ backend/src/main/java/com/example/backend/controller/AuthenticationController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService service;
