2cc5c3a0d778d16f023b157b8f30d79288c3ad10
Salih52 <saliheliack@gmail.com>
2024-10-17 00:51:23 +0300

    fix

--- backend/src/main/java/com/example/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/v1/auth/**", "/v1/assign/**", "/files/**").permitAll()
+                        .requestMatchers("/v1/auth/**", "/v1/assign/**", "/files/**" , "/error/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exceptions -> exceptions
