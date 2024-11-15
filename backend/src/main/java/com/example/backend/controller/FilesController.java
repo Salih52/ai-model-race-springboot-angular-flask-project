5b49fb7103f8cc9adf9b7ab20cba74ad60af76ec
Salih52 <saliheliack@gmail.com>
2024-11-15 19:06:28 +0300

    log

--- backend/src/main/java/com/example/backend/controller/FilesController.java
+++ backend/src/main/java/com/example/backend/controller/FilesController.java
@@ -188,7 +188,7 @@ public class FilesController {
     public ResponseEntity<Resource> getFileAdmin(@PathVariable String assignTitle,
                                             @PathVariable String filename) {
         Resource file = storageService.loadAdmin(assignTitle, filename);
-        log.info(String.valueOf(file));
+        log.info("!!!!!resurce deeri:  "+ file);
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
